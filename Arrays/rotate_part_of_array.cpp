0be8f6ebca9e4b7e8e1fd6442794a480b05b217e
Alexey <lavrik-aleksey-1@yandex.ru>
2025-10-15 10:02:34 +0300

    new tasks

--- Arrays/rotate_part_of_array.cpp
+++ Arrays/rotate_part_of_array.cpp
@@ -2,6 +2,15 @@
 
 class Solution {
 public:
+    void rotate(std::vector<int>& nums, int k) {
+        size_t n = nums.size();
+        k %= n;
+
+        reverseArray(nums, 0, n - 1);
+        reverseArray(nums, 0, k - 1);
+        reverseArray(nums, k, n - 1);
+    }
+
     void reverseArray(std::vector<int>& nums, int l, int r) {
         while (l < r) {
             int temp = nums[l];
